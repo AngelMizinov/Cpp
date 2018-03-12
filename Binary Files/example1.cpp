e3abdb0e9866d64f620bc5325321bc5f01b5927f
Angel Mizinov <angelmizinov@abv.bg>
2018-03-12 23:58:26 +0200

    Binary Files

---
+++ Binary Files/example1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	std::ofstream ofs("numbers.bin", std::ios::binary | std::ios::trunc);
+
+	if (!ofs.is_open())
+	{
+		cout << "ERROR!Cannot open the file!\n";
+		return 1;
+	}
+
+	int N;
+	cout << "Enter N:\n";
+	cin >> N;
+	
+	int num;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> num;
+		ofs.write((const char*)&num, sizeof(int));
+
+		if (!ofs.good())
+		{
+			cout << "Error while writing in the file!\n";
+			break;
+		}
+	}
+
+	ofs.close();
+
+	std::ifstream ifs("numbers.bin", std::ios::binary);
+
+	if (!ifs.is_open())
+	{
+		cout << "ERROR!Cannot open the file!\n";
+		return 1;
+	}
+
+	int buffer;
+	while (!ifs.eof())
+	{
+		ifs.seekg(2 * sizeof(int), std::ios::cur);
+
+		ifs.read((char*)&buffer, sizeof(int));
+
+		if (ifs.eof())
+			break;
+
+		if (!ifs.good())
+		{
+			cout << "Error while reading in the file!\n";
+			break;
+		}
+
+		cout << buffer << " ";
+	}
+	
+	ifs.close();
+
+	return 0;
+}
